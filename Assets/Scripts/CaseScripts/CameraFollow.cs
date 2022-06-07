09cb7702265cf7e0625fe28f8d492e1e85dde534
blaugranaa <aysenur.fcb@gmail.com>
2022-06-07 13:07:29 +0300

    updated

---
+++ Assets/Scripts/CaseScripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] GameObject character;
+
+     Vector3 offset;
+
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        offset = transform.position - character.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = character.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/CaseScripts/Character/CharacterMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    private void FixedUpdate()
+    {
+        gameObject.transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
+    }
+}
