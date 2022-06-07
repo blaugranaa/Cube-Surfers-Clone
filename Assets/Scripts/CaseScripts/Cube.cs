46e985221df3da63ed475666d41a6a3d121ef3d2
blaugranaa <aysenur.fcb@gmail.com>
2022-06-07 13:36:52 +0300

    PoolingSystem

---
+++ Assets/Scripts/CaseScripts/Cube.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PoolingSystem.Instance.InstantiateAPS("cube", transform.position);
+    }
+}
---
+++ Assets/Scripts/CaseScripts/Interface/IComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IComponent
+{
+    GameObject gameObject { get; }
+    Transform transform { get; }
+    Component component { get; }
+    bool isDestroyed { get; }
+}
