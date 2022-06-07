1a4acb7f269e9f13439e7acc31ddca99e9357cfa
blaugranaa <aysenur.fcb@gmail.com>
2022-06-07 16:01:44 +0300

    updated

--- Assets/Scripts/CaseScripts/Cube.cs
+++ Assets/Scripts/CaseScripts/Cube.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class Cube : CollectableBase
 {
-
+    public bool isCollected;
     public override void Collect(Collector collector)
     {
         base.Collect(collector);
-        collector.transform.root.GetComponentInChildren<CubeStackControl>().AddStack(gameObject);
-        //Destroy(gameObject);
-        Debug.Log("sdfsdf");
+        if (!isCollected)
+        {
+            collector.transform.root.GetComponent<CubeStackControl>().AddStack(gameObject);
+            //Destroy(gameObject);
+            isCollected = true;
+            Debug.Log("sdfsdf");
+        }
+       
     }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Cube cube = other.GetComponent<Cube>();
+    //    if (cube != null && !isCollected && cube.isCollected)
+    //    {
+    //        isCollected = true;
+    //        transform.root.GetComponent<CubeStackControl>().AddStack(gameObject);
+    //    }
+
+    //}
     //private void OnTriggerEnter(Collider other)
     //{
     //    PoolingSystem.Instance.InstantiateAPS("cube", transform.position);
